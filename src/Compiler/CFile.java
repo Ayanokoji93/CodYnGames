0eaaac0a01a7ff74bc065198e5cdefa321d86adb
Ayanokoji93 <124360610+Ayanokoji93@users.noreply.github.com>
2024-05-23 17:45:20 +0200

    Update CFile

--- src/Compiler/CFile.java
+++ src/Compiler/CFile.java
@@ -3,6 +3,7 @@ package Compiler;
 import Compiler.factor.GeneralCompiler;
 
 import java.io.*;
+import java.util.List;
 
 public class CFile extends GeneralCompiler {
 
@@ -19,11 +20,13 @@ public class CFile extends GeneralCompiler {
     }
 
     @Override
-    public String execute(String code) throws IOException, InterruptedException {
+    public String execute(String code, List<Integer> numbers) throws IOException, InterruptedException {
         writeResponseInFile(code);
+
 
         String compileCommand = "C:\\msys64\\mingw64\\bin\\gcc.exe -o C:\\Users\\Fayçal\\Desktop\\JavaProject\\CodYnGames\\tempFile\\tempExecutable " + getPathFile();
         Process compileProcess = Runtime.getRuntime().exec(compileCommand);
+
 
         BufferedReader compileErrorReader = new BufferedReader(new InputStreamReader(compileProcess.getErrorStream()));
         StringBuilder compileErrorOut = new StringBuilder();
@@ -33,20 +36,32 @@ public class CFile extends GeneralCompiler {
         }
         compileErrorReader.close();
 
+
         int compileResult = compileProcess.waitFor();
         if (compileResult != 0) {
             deleteTempFile();
             return "Compilation Error:\n" + compileErrorOut.toString();
         }
 
+
         Process execProcess = Runtime.getRuntime().exec("C:\\Users\\Fayçal\\Desktop\\JavaProject\\CodYnGames\\tempFile\\tempExecutable");
 
-        BufferedReader inputReader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()));
-        StringBuilder inputOut = new StringBuilder();
-        while ((line = inputReader.readLine()) != null) {
-            inputOut.append(line).append("\n");
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(execProcess.getOutputStream()));
+        for (Integer number : numbers) {
+            writer.write(number.toString());
+            writer.newLine();
         }
-        inputReader.close();
+        writer.close();
+
+
+        StringBuilder inputOut = new StringBuilder();
+        try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()))) {
+            while ((line = inputReader.readLine()) != null) {
+                inputOut.append(line).append("\n");
+            }
+        }
+
 
         deleteTempExecFile();
         deleteTempFile();
