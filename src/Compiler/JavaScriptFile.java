6f8f5c687e560fef3f0d98fed9f1f5d9c23fea64
Ayanokoji93 <124360610+Ayanokoji93@users.noreply.github.com>
2024-05-23 17:51:17 +0200

    Update JavaScriptFile

--- src/Compiler/JavaScriptFile.java
+++ src/Compiler/JavaScriptFile.java
@@ -3,6 +3,7 @@ package Compiler;
 import Compiler.factor.GeneralCompiler;
 
 import java.io.*;
+import java.util.List;
 
 public class JavaScriptFile extends GeneralCompiler {
     private String fileName;
@@ -12,35 +13,42 @@ public class JavaScriptFile extends GeneralCompiler {
     }
 
     @Override
-    public String execute(String code) throws IOException, InterruptedException {
-
+    public String execute(String code, List<Integer> numbers) throws IOException, InterruptedException {
         writeResponseInFile(code);
 
-        String nodeExecutable = "C:\\Program Files\\nodejs\\node.exe ";
-        String command = nodeExecutable + getPathFileJs();
+
+        String nodeExecutable = "C:\\Program Files\\nodejs\\node.exe";
+
+
+        String command = nodeExecutable + " " + getPathFileJs();
 
 
         Process execProcess = Runtime.getRuntime().exec(command);
+
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(execProcess.getOutputStream()));
+        for (Integer number : numbers) {
+            writer.write(number.toString());
+            writer.newLine();
+        }
+        writer.close();
+
+
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(System.lineSeparator());
+            }
+        }
+
+
         execProcess.waitFor();
 
-        BufferedReader inputReader = new BufferedReader(new InputStreamReader(execProcess.getInputStream()));
-        StringBuilder inputOut = new StringBuilder();
-        String line;
-        while ((line = inputReader.readLine()) != null) {
-            inputOut.append(line).append("\n");
-        }
-        inputReader.close();
-
-        BufferedReader errorReader = new BufferedReader(new InputStreamReader(execProcess.getErrorStream()));
-        StringBuilder errorOut = new StringBuilder();
-        while ((line = errorReader.readLine()) != null) {
-            errorOut.append(line).append("\n");
-        }
-        errorReader.close();
 
         deleteTempFile();
 
-        return inputOut.toString() + errorOut.toString();
+        return output.toString();
     }
     public String getPathFileJs() {
         return tempFile.getAbsolutePath();
