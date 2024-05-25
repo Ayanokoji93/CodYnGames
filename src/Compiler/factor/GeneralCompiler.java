447a8165cf91c78ba5e765bb1dd8a8f0838ce0f9
SacaDoo <romanthoyer9@gmail.com>
2024-05-25 21:49:35 +0200

    Update GeneralCompiler.java

--- src/Compiler/factor/GeneralCompiler.java
+++ src/Compiler/factor/GeneralCompiler.java
@@ -2,15 +2,28 @@ package Compiler.factor;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.List;
 
+/**
+ * The GeneralCompiler abstract class provides common functionality for compiler classes.
+ */
 public abstract class GeneralCompiler {
     protected File tempFile;
     protected String response;
 
+    /**
+     * Constructor that creates a temporary file with the specified extension.
+     *
+     * @param extension the file extension for the temporary file.
+     * @throws IOException if an I/O error occurs while creating the temporary file.
+     */
     public GeneralCompiler(String extension) throws IOException {
-        this.tempFile = File.createTempFile("temp", extension, new File("C:\\Users\\Fayçal\\Desktop\\JavaProject\\CodYnGames\\tempFile"));
+        this.tempFile = this.tempFile = File.createTempFile("temp", extension, new File("C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\tempFile"));
     }
 
+    /**
+     * Prompt the user to write a response and store it.
+     */
     public void askResponse(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write your answer below :");
@@ -22,6 +35,12 @@ public abstract class GeneralCompiler {
         response = str.toString();
     }
 
+    /**
+     * Write the provided response to the temporary file.
+     *
+     * @param response the response to write to the file.
+     * @throws IOException if an I/O error occurs while writing to the file.
+     */
     public void writeResponseInFile(String response) throws IOException {
         if (response != null) {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.tempFile))) {
@@ -32,22 +51,60 @@ public abstract class GeneralCompiler {
         }
     }
 
+    /**
+     * Delete the temporary file.
+     */
     public void deleteTempFile() {
         if(this.tempFile.exists()){
             this.tempFile.delete();
         }
     }
 
+    /**
+     * Get the stored response.
+     *
+     * @return the stored response.
+     */
     public String getResponse(){
         return this.response;
     }
 
+    /**
+     * Get the absolute path of the temporary file.
+     *
+     * @return the absolute path of the temporary file.
+     */
     public String getPathFile(){
         return tempFile.getAbsolutePath();
     }
 
-    public abstract String execute(String code) throws IOException, InterruptedException;
+    /**
+     * Adds the content of a source file to the end of the current file.
+     *
+     * @param sourceFilePath the path to the source file.
+     * @throws IOException if an I/O error occurs.
+     */
+    public void addContentToFile(String sourceFilePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.tempFile, true));
+             BufferedReader reader = new BufferedReader(new FileReader(sourceFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+        //deleteTempFile();
+    }
 
-    public abstract String exercisesWNumber(String number1, String number2) throws IOException, InterruptedException;
+    /**
+     * Abstract method to be implemented by subclasses to execute code.
+     *
+     * @param code the code to execute.
+     * @param numbers a list of numbers to use as input.
+     * @return the output of the execution.
+     * @throws IOException if an I/O error occurs.
+     * @throws InterruptedException if the execution is interrupted.
+     */
+    public abstract String execute(String code, List<Integer> numbers) throws IOException, InterruptedException;
 
 }
