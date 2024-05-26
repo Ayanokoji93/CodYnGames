cd324b344189a647f8c1d683aa2c921630f9c6a1
Ayanokoji93 <124360610+Ayanokoji93@users.noreply.github.com>
2024-05-26 23:25:02 +0200

    Projet Final

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -1,25 +1,27 @@
 package Classes;
 
-import Compiler.*;
-import Compiler.factor.GeneralCompiler;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
+/**
+ * The Main class represents the main entry point of the application.
+ */
 public class Main extends Application {
 
+    // Parameters of the window
     private final String WINDOW_TITLE = "CodYnGame";
-    private final double WINDOW_WIDTH = 960;
-    private final double WINDOW_HEIGHT = 540;
+    private final double WINDOW_WIDTH = 1920;
+    private final double WINDOW_HEIGHT = 1080;
 
+    /**
+     * Starts the JavaFX application.
+     *
+     * @param primaryStage the primary stage of the application.
+     * @throws Exception if an error occurs during the run of the application.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene mainViewScene = new Scene(new Window(10));
@@ -35,57 +37,14 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Main method, the entry point of the application.
+     *
+     * @param args the command-line arguments.
+     * @throws IOException if an I/O error occurs.
+     * @throws InterruptedException if the process is interrupted.
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
         launch(args);
-
-        /*StdinStdout generateExecutor = new StdinStdout("C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\correctionFile\\generateNumberExo4.py");
-        PythonFile test = new PythonFile();
-        test.askResponse();
-        test.writeResponseInFile(test.getResponse());
-        String sourceFilePath = test.getPathFile();
-        String targetFilePath = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\mainInclude\\mainExo5.py";
-        test.addContentToFile(targetFilePath);
-        List<Integer> list = generateExecutor.executeScript();
-
-        StdinStdout test2 = new StdinStdout(test.getPathFile());
-        System.out.println(test2.executeScriptWithArgs(list));*/
     }
 }
-        /*CFile test2 = new CFile();
-        test2.askResponse();
-        String sourceFilePath2 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\tempFile\\temp.c";
-        String targetFilePath2 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\mainInclude\\mainExo5.c";
-        Include.addContentToFile(sourceFilePath2, targetFilePath2);
-        JavaFile test3 = new JavaFile();
-        test3.askResponse();
-        String sourceFilePath3 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\tempFile\\temp.java";
-        String targetFilePath3 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\mainInclude\\mainExo5.java";
-        Include.addContentToFile(sourceFilePath3, targetFilePath3);
-        JavaScriptFile test4 = new JavaScriptFile();
-        test4.askResponse();
-        String sourceFilePath4 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\tempFile\\temp.py";
-        String targetFilePath4 = "C:\\Users\\FiercePC\\Desktop\\JavaProject\\CodYnGames\\mainInclude\\mainExo5.js";
-        Include.addContentToFile(sourceFilePath4, targetFilePath4);*/
-
-
-        /*StdinStdout generateExecutor = new StdinStdout(generateScriptPath);
-        List<Integer> numbers = generateExecutor.executeScript();
-        for (Integer number : numbers) {
-            System.out.println("Nombre: " + number);
-        }
-
-        PythonFile executorPythonFile = new PythonFile();
-        executorPythonFile.askResponse();
-        String userResult = executorPythonFile.execute(executorPythonFile.getResponse(), numbers);
-
-        StdinStdout correctionExecutor = new StdinStdout(correctionScriptPath);
-        String correctionResult = correctionExecutor.executeScriptWithArgs(numbers);
-
-        System.out.println("Résultat de l'utilisateur: " + userResult);
-        System.out.println("Résultat de la correction: " + correctionResult);
-
-        if (userResult.trim().equalsIgnoreCase(correctionResult.trim())) {
-            System.out.println("Résultat correct.");
-        } else {
-            System.out.println("Pas correct.");
-        }*/
