786b6e8212458e3db84038af5b40d7663fea5f3b
Ayanokoji93 <124360610+Ayanokoji93@users.noreply.github.com>
2024-05-26 23:32:05 +0200

    Projet Final

--- src/data.sql
+++ src/data.sql
@@ -1,14 +1,6 @@
 INSERT INTO Exercises (title , description , code_languages) VALUES
-('Produit nombre entier', 'Demander à l’utilisateur deux nombres, les mémoriser dans deux variables, multiplier leurs valeurs en affectant le résultat à une troisième variable, puis l’afficher.' , 'C'),
-('Addition de deux nombres entiers', 'Demander à l’utilisateur deux nombres entiers, additionnez-les et affichez le résultat.', 'Python'),
-('Affichage de message', 'Afficher un message "Bonjour !" à l’utilisateur.', 'PHP'),
-('Calcul de la factorielle en JavaScript', 'Demander à l’utilisateur un nombre entier positif, puis calculer et afficher sa factorielle en utilisant JavaScript.', 'JavaScript');
-
-
-
-
-
-
-
-
-
+('Produit de nombres entiers', 'Demander à l’utilisateur deux nombres, les mémoriser dans deux variables, multiplier leurs valeurs en affectant le résultat à une troisième variable, puis l’afficher.' , 'Python, C, Java, JavaScript, PHP'),
+('Calcul d''une factorielle', 'Demander à l''utilisateur un nombre, et calcul sa factorielle.', 'Python, C, Java, JavaScript, PHP'),
+('Calcul d''une moyenne', 'Écrire un programme qui calcul la moyenne de 10 notes.', 'Python, C, Java, JavaScript, PHP'),
+('Inverse d''un nombre', 'Écrire un programme qui permet d''inverser les chiffres d''un entier N. Par exemple  N=35672, le résultat affiché doit être   27653', 'Python, C, Java, JavaScript, PHP'),
+('Calculer nombre de chiffre d''un entier', 'Écrire un programme qui permet de calculer le nombre de chiffres d''un entier N saisi par l''utilisateur.  Exemple  N = 10843 doit retourner 5.', 'Python, Java, JavaScript, PHP');
